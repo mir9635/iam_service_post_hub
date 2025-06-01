042313d9c6e8017c007b2a74f769dfdbdde9f50f
Anton Trapeznikov <mir9635@yandex.ru>
2025-06-01 19:13:30 +0300

    add UserRegistrationImpl

--- src/main/java/com/post_hub/iam_service/controller/AuthController.java
+++ src/main/java/com/post_hub/iam_service/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.post_hub.iam_service.controller;
 import com.post_hub.iam_service.model.constants.ApiLogMessage;
 import com.post_hub.iam_service.model.request.user.LoginRequest;
 import com.post_hub.iam_service.model.dto.user.UserProfileDTO;
+import com.post_hub.iam_service.model.request.user.RegistrationUserRequest;
 import com.post_hub.iam_service.model.response.IamResponse;
 import com.post_hub.iam_service.service.AuthService;
 import com.post_hub.iam_service.utils.ApiUtils;
@@ -41,6 +42,19 @@ public class AuthController {
         log.trace(ApiLogMessage.NAME_OF_CURRENT_METHOD.getValue(), ApiUtils.getMethodName());
 
         IamResponse<UserProfileDTO> result = authService.refreshAccessToken(refreshToken);
+        Cookie authorizationCookie = ApiUtils.createAuthCookie(result.getPayload().getToken());
+        response.addCookie(authorizationCookie);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("${end.point.register}")
+    public ResponseEntity<?> register(
+            @RequestBody @Valid RegistrationUserRequest request,
+            HttpServletResponse response) {
+        log.trace(ApiLogMessage.NAME_OF_CURRENT_METHOD.getValue(), ApiUtils.getMethodName());
+
+        IamResponse<UserProfileDTO> result = authService.registerUser(request);
         Cookie authorizationCookie = ApiUtils.createAuthCookie(result.getPayload().getToken());
         response.addCookie(authorizationCookie);
 
--- src/main/java/com/post_hub/iam_service/service/AuthService.java
+++ src/main/java/com/post_hub/iam_service/service/AuthService.java
@@ -2,6 +2,7 @@ package com.post_hub.iam_service.service;
 
 import com.post_hub.iam_service.model.request.user.LoginRequest;
 import com.post_hub.iam_service.model.dto.user.UserProfileDTO;
+import com.post_hub.iam_service.model.request.user.RegistrationUserRequest;
 import com.post_hub.iam_service.model.response.IamResponse;
 
 public interface AuthService {
@@ -9,4 +10,6 @@ public interface AuthService {
     IamResponse<UserProfileDTO> login(LoginRequest request);
 
     IamResponse<UserProfileDTO> refreshAccessToken(String refreshToken);
+
+    IamResponse<UserProfileDTO> registerUser(RegistrationUserRequest request);
 }
