1cc490c68bd99aca6f7a770e376103495dd8cdbc
Anton Trapeznikov <mir9635@yandex.ru>
2025-06-04 19:05:06 +0300

    add CommentServiceDTO

---
+++ src/main/java/com/post_hub/iam_service/model/dto/post/PostOwnerDTO.java
@@ -0,0 +1,17 @@
+package com.post_hub.iam_service.model.dto.post;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostOwnerDTO implements Serializable {
+
+    private Integer id;
+    private String username;
+    private String email;
+}
